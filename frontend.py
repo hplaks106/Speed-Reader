88672da49241a869ad2c52c27809ad163b0f2e29
Tyler <tlecluse2018@my.fit.edu>
2020-11-21 19:30:00 -0500

    Refactored the button creation from create_widget to initUI. Added the Loading bar utilizing multithreading.

--- frontend.py
+++ frontend.py
@@ -1,7 +1,12 @@
 """Program converts PDF file to text and displays one word at a time."""
 import tkinter as tk
+from tkinter import *
 from tkinter import StringVar
 from tkinter import filedialog
+from tkinter import Tk, HORIZONTAL
+from tkinter.ttk import Frame, Button, Label, Progressbar, Style
+import threading
+import time
 
 
 class Application(tk.Frame):
@@ -9,36 +14,48 @@ class Application(tk.Frame):
         """Initialization creates GUI, widgets, and variables."""
         super().__init__(master)
         self.master = master
-        self.pack()
-        self.create_widgets()
+        self.initUI()
         self.count = 0
         self.txt_speed = 0
         self.file = None
-        # self.textLabel.after(1000, self.display_text)
 
-    def create_widgets(self):
-        """Creates tkinter widgets for the GUI."""
+    def initUI(self):
+        """Initialize the User Interface"""
         global var
         var = StringVar()
+        self.master.title("Speed-Reader")
 
-        self.textLabel = tk.Label(self, textvariable=var)
-        self.textLabel.pack(side="top")
-        # Button to Restart Text
-        self.restart = tk.Button(self, text="Restart Text", fg="black",
-                                 command=self.restart_txt)
-        self.restart.pack(side="bottom")
-        # Button to Play/Pause
-        self.pause = tk.Button(self, text="PLAY/PAUSE", fg="black",
-                               command=self.pause_txt)
-        self.pause.pack(side="bottom")
-        # Button to terminate program
-        self.quit = tk.Button(self, text="QUIT", fg="black",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
-        # Button to add local file system
-        self.addFile = tk.Button(self, text='Open', fg="black",
-                                 command=self.UploadAction)
-        self.addFile.pack(side="bottom")
+        Style().configure("Tbutton", padding=(0,5,0,5), font="serif 10")
+        self.columnconfigure(0, pad=5, minsize=50)
+
+        self.rowconfigure(0, pad=5, minsize=25)
+        self.rowconfigure(1, pad=5, minsize=25)
+        self.rowconfigure(2, pad=5, minsize=25)
+        self.rowconfigure(3, pad=5, minsize=25)
+        self.rowconfigure(4, pad=5, minsize=25)
+        self.rowconfigure(5, pad=5, minsize=25)
+
+        self.textLabel = Label(self, textvariable=var)
+        self.textLabel.grid(row=0, column=0)
+
+        self.restart = Button(self, text="Restart Text",
+                              command=self.restart_txt)
+        self.restart.grid(row=1, column=0)
+
+        self.pause = Button(self, text="PLAY/PAUSE", command=self.pause_txt)
+        self.pause.grid(row=2, column=0)
+
+        self.quit = Button(self, text="QUIT", command=self.master.destroy)
+        self.quit.grid(row=3, column=0)
+
+        self.addFile = Button(self, text="OPEN", command=self.UploadAction)
+        self.addFile.grid(row=4, column=0)
+
+        self.progress = Progressbar(self, orient=HORIZONTAL, length=250,
+                                    mode='determinate')
+        self.textLabel.after(1000, self.display_text)
+
+        self.pack()
 
     def display_text(self):
         """Temporary test case sentence output."""
@@ -69,10 +86,18 @@ class Application(tk.Frame):
         filename = filedialog.askopenfilename()
         self.file = open(filename, "r")
         print('Selected:', filename)
+        def real_traitement():
+            self.progress.grid(row=5, column=0)
+            self.progress.start()
+            time.sleep(5)
+            self.progress.stop()
+            self.progress.grid_forget()
+            self.addFile['state'] = 'normal'
+        self.addFile['state'] = 'disabled'
+        threading.Thread(target=real_traitement).start()
 
 
 root = tk.Tk()
 root.geometry("500x500")
 app = Application(master=root)
 app.mainloop()
-print("Fuck you hunter!")
