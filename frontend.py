e15a371bb1803fe417cc15447c01a49a1fc469b6
ChrisHayo <chaworth2018@my.fit.edu>
2020-11-24 19:11:22 -0500

    Changing the look of the buttons and size of text

--- frontend.py
+++ frontend.py
@@ -24,38 +24,49 @@ class Application(Frame):
         columns = [0, 1, 2, 3, 4]  # columns grid padding
         rows = [0, 1, 2, 3, 4, 5]  # rows grid padding
         var = StringVar()
+        style=Style()
         self.master.title("Speed-Reader")
 
-        Style().configure("Tbutton", padding=(0, 5, 0, 5), font="serif 10")
+        Style().configure("Tbutton", padding=(0, 5, 0, 5))
+        style.configure('Q.TButton', foreground='red')
+        style.configure('O.TButton', foreground='green')
+        style.configure('P.TButton', foreground='blue')
+        style.configure('T.TButton', font=('serif', 30),
+                                foreground = 'black')
+
         self.columnconfigure(columns, pad=5, minsize=25)
         self.rowconfigure(rows, pad=5, minsize=25)
 
-        self.textLabel = Label(self, width=25, textvariable=var)
-        self.textLabel.configure(anchor="center")
-        self.textLabel.grid(row=0, column=2)
+        self.textLabel = Label(self, style='T.TButton',
+                               textvariable=var)
+
+        self.textLabel.grid(row=0, column=3)
 
         self.restart = Button(self, text="Restart Text",
                               command=self.restart_txt)
-        self.restart.grid(row=1, column=2)
+        self.restart.grid(row=2, column=2)
 
-        self.pause = Button(self, text="PLAY/PAUSE", command=self.pause_txt)
-        self.pause.grid(row=2, column=2)
+        self.pause = Button(self, text="⏯️",  style='P.TButton',
+                            command=self.pause_txt)
+        self.pause.grid(row=1, column=4)
 
         init_slide_val = DoubleVar()  # create initial slider value variable
         self.speed_slider = Scale(self, cursor='sb_h_double_arrow', from_=100,
                                   to=300, length=200, tickinterval=50,
-                                  resolution=50, orient='horizontal',
+                                  resolution=5, orient='horizontal',
                                   variable=init_slide_val, command=lambda x:
                                   self.change_speed(self.speed_slider.get()))
 
         init_slide_val.set(200)  # initialize the slider to 200 WPM
-        self.speed_slider.grid(row=3, column=2)
+        self.speed_slider.grid(row=3, column=3)
 
-        self.quit = Button(self, text="QUIT", command=self.master.destroy)
-        self.quit.grid(row=4, column=2)
+        self.quit = Button(self, text="QUIT", style='Q.TButton',
+                           command=self.master.destroy)
+        self.quit.grid(row=2, column=4)
 
-        self.addFile = Button(self, text="OPEN", command=self.UploadAction)
-        self.addFile.grid(row=5, column=2)
+        self.addFile = Button(self, text="OPEN", style='O.TButton',
+                              command=self.UploadAction)
+        self.addFile.grid(row=1, column=2)
 
         self.textLabel.after(1000, self.display_text)
 
@@ -85,9 +96,7 @@ class Application(Frame):
         """Changes the text display speed in 60 WPM increments"""
         speed = int((60 / speed) * 1000)  # convert WPM to text_speed
         self.saved_speed = speed
-        # if the text isn't paused
-        if self.txt_speed != 0:
-            self.txt_speed = self.saved_speed
+        self.txt_speed = self.saved_speed
 
     def restart_txt(self):
         """Resets the displayed text to the beginning of the file."""
